b3a7ade84c0743ab97e28f89db2741da03a80c02
Hafizh <110800141+MuhammadHafizhRaharja@users.noreply.github.com>
2024-10-09 20:10:23 +0700

    TugasPercabangan
    Muhammad Hafizh Raharja
    607012400055

--- TugasPercabangan.java
+++ TugasPercabangan.java
@@ -4,15 +4,38 @@ public class TugasPercabangan{
     public static void main(String[] args){
         int lamaKerja, jumlahJamKerja;
         double totalGaji;
+        double tarifLemburPerJam = 50000;
+        int jamRegulerPerMinggu = 40;
+        int jamLembur = 0;
         
-
         Scanner scanner = new Scanner(System.in);
         lamaKerja       = scanner.nextInt();
         jumlahJamKerja  = scanner.nextInt();
 
-        /* 
-            Tuliskan proses perhitungan gaji di sini
-        */
+        if(lamaKerja < 5 && lamaKerja >= 0) {
+            totalGaji = 5000000;
+        } else if (lamaKerja <= 10 && lamaKerja >= 5) {
+            totalGaji = 5000000 + (5000000 * 5.0/100);
+        } else if (lamaKerja > 10) {
+            totalGaji = 5000000 + (5000000 * 10.0/100);
+        } else {
+            System.out.println("Input lama kerja dengan benar!");
+            return;
+        }
+
+        if (jumlahJamKerja > jamRegulerPerMinggu) {
+            jamLembur = jumlahJamKerja - jamRegulerPerMinggu;
+        } else if (jumlahJamKerja < 40) {
+            System.out.println("Kerja lebih keras!");
+            return;
+        }
+        
+        // Menghitung uang lembur
+        double uangLembur = jamLembur * tarifLemburPerJam;
+        
+        // Menghitung total gaji
+        totalGaji = totalGaji + uangLembur;
+
         
         System.out.println(totalGaji);
     }
